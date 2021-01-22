a54132d0353c665357f06c7ff6a6b29729e54cd2
Jacob Gogichaishvili <jggsecondary@gmail.com>
2021-01-22 16:05:24 +0400

    Extract theme into another file

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,10 @@ import {
   Card,
   Divider,
   Grid,
-  ThemeProvider,
   Container,
-  CssBaseline,
 } from "@material-ui/core";
-import {
-  makeStyles,
-  createStyles,
-  createMuiTheme,
-} from "@material-ui/core/styles";
+import { makeStyles, createStyles } from "@material-ui/core/styles";
 import { ArrowUpward, ArrowDownward, ImportExport } from "@material-ui/icons";
-
 const useStyles = makeStyles(() =>
   createStyles({
     card: {
@@ -49,85 +42,74 @@ const useStyles = makeStyles(() =>
 function App() {
   const [repeaterCount, setRepeaterCount] = useState(1);
   const classes = useStyles();
-  const [darkState] = useState(true);
-  const palletType = darkState ? "dark" : "light";
-  const darkTheme = createMuiTheme({
-    palette: {
-      type: palletType,
-    },
-  });
 
   return (
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <Container>
-        <div className="App">
-          <Card className={classes.card}>
-            <Grid container spacing={2}>
-              <Grid item sm={6} xs={12} className={classes.flexCenter}>
-                <Typography variant="body1">
-                  <Typography variant="h5">The Repeater Calculator</Typography>
-                </Typography>
-              </Grid>
-              <Grid item sm={3} xs={6} className={classes.flexCenter}>
-                <Typography variant="body1">
-                  {repeaterCount * 1} repeater{repeaterCount > 1 ? "s" : ""}
-                </Typography>
-              </Grid>
-              <Grid item sm={3} xs={6} className={classes.flexCenter}>
-                <ButtonGroup variant="contained">
-                  <Button onClick={() => addCount(1)}>
-                    <ArrowUpward />
-                  </Button>
-                  <Button onClick={() => addCount(-1)}>
-                    <ArrowDownward />
-                  </Button>
-                  <Button onClick={textBoxInput}>
-                    <ImportExport />
-                  </Button>
-                </ButtonGroup>
-              </Grid>
+    <Container>
+      <div className="App">
+        <Card className={classes.card}>
+          <Grid container spacing={2}>
+            <Grid item sm={6} xs={12} className={classes.flexCenter}>
+              <Typography variant="body1">
+                <Typography variant="h5">The Repeater Calculator</Typography>
+              </Typography>
             </Grid>
-            <Divider className={classes.divider} />
-            <Grid container spacing={2}>
-              <Grid item className={classes.bottomSection}>
-                <Typography variant="body1">Final ingredients</Typography>
-                <Typography variant="body1">
-                  {prettyStacks(repeaterCount * 3)} stone
-                </Typography>
-                <Typography variant="body1">
-                  {prettyStacks(repeaterCount * 2)} redstone torches
-                </Typography>
-                <Typography variant="body1">
-                  {prettyStacks(repeaterCount * 1)} redstone dust
-                </Typography>
-              </Grid>
-              <Grid item className={classes.bottomSection}>
-                <Typography variant="body1">Raw ingredients</Typography>
-                <Typography variant="body1">
-                  {prettyStacks(repeaterCount * 3)} stone
-                </Typography>
-                <Typography variant="body1">
-                  {prettyStacks(Math.ceil(repeaterCount / 2) * 2)} wooden
-                  planks*
-                </Typography>
-                <Typography variant="body1">
-                  {prettyStacks(repeaterCount * 3)} redstone dust
-                </Typography>
-              </Grid>
+            <Grid item sm={3} xs={6} className={classes.flexCenter}>
+              <Typography variant="body1">
+                {repeaterCount * 1} repeater{repeaterCount > 1 ? "s" : ""}
+              </Typography>
             </Grid>
-            <Divider className={classes.divider} />
-            <Typography variant="body1" className={classes.planksNote}>
-              *wooden planks are rounded up
-            </Typography>
-          </Card>
-          <Typography variant="body1" className={classes.madeBy}>
-            Made by Honbra <br /> Special thanks to Tokfrans03 for the item
-            displays and StayWithMeSenpai for the rounding.
+            <Grid item sm={3} xs={6} className={classes.flexCenter}>
+              <ButtonGroup variant="contained">
+                <Button onClick={() => addCount(1)}>
+                  <ArrowUpward />
+                </Button>
+                <Button onClick={() => addCount(-1)}>
+                  <ArrowDownward />
+                </Button>
+                <Button onClick={textBoxInput}>
+                  <ImportExport />
+                </Button>
+              </ButtonGroup>
+            </Grid>
+          </Grid>
+          <Divider className={classes.divider} />
+          <Grid container spacing={2}>
+            <Grid item className={classes.bottomSection}>
+              <Typography variant="body1">Final ingredients</Typography>
+              <Typography variant="body1">
+                {prettyStacks(repeaterCount * 3)} stone
+              </Typography>
+              <Typography variant="body1">
+                {prettyStacks(repeaterCount * 2)} redstone torches
+              </Typography>
+              <Typography variant="body1">
+                {prettyStacks(repeaterCount * 1)} redstone dust
+              </Typography>
+            </Grid>
+            <Grid item className={classes.bottomSection}>
+              <Typography variant="body1">Raw ingredients</Typography>
+              <Typography variant="body1">
+                {prettyStacks(repeaterCount * 3)} stone
+              </Typography>
+              <Typography variant="body1">
+                {prettyStacks(Math.ceil(repeaterCount / 2) * 2)} wooden planks*
+              </Typography>
+              <Typography variant="body1">
+                {prettyStacks(repeaterCount * 3)} redstone dust
+              </Typography>
+            </Grid>
+          </Grid>
+          <Divider className={classes.divider} />
+          <Typography variant="body1" className={classes.planksNote}>
+            *wooden planks are rounded up
           </Typography>
-        </div>
-      </Container>
-    </ThemeProvider>
+        </Card>
+        <Typography variant="body1" className={classes.madeBy}>
+          Made by Honbra <br /> Special thanks to Tokfrans03 for the item
+          displays and StayWithMeSenpai for the rounding.
+        </Typography>
+      </div>
+    </Container>
   );
 
   function addCount(i: number) {
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { theme } from "./theme";
+
+function Root() {
+  const [darkState] = useState(true);
+  const palletType = darkState ? "dark" : "light";
+  return (
+    <ThemeProvider theme={theme(palletType)}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  );
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root></Root>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
